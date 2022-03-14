60cd483246972be1d69cf604ea739d8465fe249a
rikodwi <riko.aa.aa.aa.aa@gmail.com>
2022-03-14 17:09:50 +0700

    task3 manual

---
+++ array2.php
@@ -0,0 +1,22 @@
+<?php 
+$mahasiswa = ["riko dwi setiawan", "21520018", "teknik informatika", "riko.aa.aa.aa@gmail.com"];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>daftar mahasiswa</title>
+</head>
+<body>
+    <h1>daftar mahasiswa</h1>
+    <ul>
+        <li>riko dwi setiawan</li>
+        <li>21520018</li>
+        <li>teknik informatika</li>
+        <li>teknik informatika</li>
+    </ul>
+</body>
+</html>
