28e3f31a426b347820e8e55fed057228311902cf
rikodwi <riko.aa.aa.aa.aa@gmail.com>
2022-03-14 16:15:52 +0700

    array

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+$hari = array("senin", "selasa", "rabu");
+
+//var_dump($hari);
+//echo $hari[0];
+print_r($hari);
+echo "<br>";
+
+//cara baru
+//$bulan = ["januari", "february", "maret"];
+//print_r($bulan);
+print_r($hari);
+$hari[] ="kamis";
+echo "<br>";
+print_r($hari);
+?>
